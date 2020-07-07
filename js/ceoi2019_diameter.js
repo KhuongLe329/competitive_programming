072e491ead8e24e71137e49a33584911bfba82ec
todayis-wendy <65122571+todayis-wendy@users.noreply.github.com>
2020-07-07 23:55:32 +0700

    Add files via upload

---
+++ js/ceoi2019_diameter.js
@@ -0,0 +1,8 @@
+$(".brute").click(function() {
+  $(".brute_solution .brute_show").toggle();
+});
+$(".main").click(function() {
+  $(".main_solution .main_show").toggle();
+});
+$(".brute_solution .brute_show").toggle();
+$(".main_solution .main_show").toggle();
